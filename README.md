318ebfa3ea51c48b1787a66761d5cffa86d775c7
matthewbrems <matthew.w.brems@gmail.com>
2021-02-23 09:09:45 -0500

    pushing content

---
+++ README.md
@@ -0,0 +1,14 @@
+# Streamlit App for Blood Cell Count Dataset
+
+This app allows you to upload an image of blood cells and use an [object detection model](https://models.roboflow.com/object-detection) to detect the white blood cells, red blood cells, and platelets.
+
+The app is [hosted here](https://share.streamlit.io/matthewbrems/bccd_streamlit_app/main):
+```
+https://share.streamlit.io/matthewbrems/bccd_streamlit_app/main
+```
+
+The app will work as is. The code made available here will work _except you will need your own API key_. [Create a Roboflow account](https://app.roboflow.com) to get your own API key.
+
+Want to [build and deploy your own object detection model and app](https://blog.streamlit.io/p/2289e8b8-8acf-499c-ba40-a7387f298a9d/)? Follow the steps in our "How To" blog!
+
+This app was created using [Roboflow](https://roboflow.com) and [Streamlit](https://streamlit.io/).
---
+++ streamlit_app.py
@@ -0,0 +1,126 @@
+import streamlit as st
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
+
+##########
+##### Set up sidebar.
+##########
+
+# Add in location to select image.
+
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
+
+st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/health)')
+
+## Add in sliders.
+confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
+overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
+
+
+image = Image.open('./images/roboflow_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
+image = Image.open('./images/streamlit_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
+##########
+##### Set up main app.
+##########
+
+## Title.
+st.write('# Blood Cell Count Object Detection')
+
+## Pull in default image or user-selected image.
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/roboflow-ai/bccd_streamlit_app_public/blob/main/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
+else:
+    # User-selected image.
+    image = Image.open(uploaded_file)
+
+## Subtitle.
+st.write('### Inferenced Image')
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Base 64 encode.
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')
+
+## Construct the URL to retrieve image.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ',
+    '&format=image',
+    f'&overlap={overlap_threshold * 100}',
+    f'&confidence={confidence_threshold * 100}',
+    '&stroke=2',
+    '&labels=True'
+])
+
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
+
+image = Image.open(BytesIO(r.content))
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Display image.
+st.image(image,
+         use_column_width=True)
+
+## Construct the URL to retrieve JSON.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ'
+])
+
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
+
+## Save the JSON.
+output_dict = r.json()
+
+## Generate list of confidences.
+confidences = [box['confidence'] for box in output_dict['predictions']]
+
+## Summary statistics section in main app.
+st.write('### Summary Statistics')
+st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
+st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
+
+## Histogram in main app.
+st.write('### Histogram of Confidence Levels')
+fig, ax = plt.subplots()
+ax.hist(confidences, bins=10, range=(0.0,1.0))
+st.pyplot(fig)
+
+## Display the JSON in main app.
+st.write('### JSON Output')
+st.write(r.json())
